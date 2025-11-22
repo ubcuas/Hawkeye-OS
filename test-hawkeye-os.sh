5a24ff3bc71066b256fad870ae517888b145c1a3
loolin06 <loolin08@student.ubc.ca>
2025-11-22 11:52:34 -0800

    Edits to the script

--- test-hawkeye-os.sh
+++ test-hawkeye-os.sh
@@ -12,12 +12,14 @@ fi
 SESSION=Project
 SESSIONEXISTS=(tmux list-sessions | grep $SESSION)
 
+# Make sure the sessions don't already exist
 if [ "$SESSIONEXISTS" = "" ] 
 then
 tmux new-session -d -s $SESSION 
 
 tmux rename-window -t 0 'Main'
 
+# Window 1 will launch the orchestrator node after building the ROS environment
 tmux new-window -t $SESSION:1 -n 'Orchestrator'
 if command -v docker-compose &> /dev/null; then
 tmux send-keys -t $SESSION: 'source ~/.bashrc' C-m 'PROGRAM=orchestrator' C-m '
@@ -37,6 +39,7 @@ source install/setup.bash &&
 ros2 run orchestrator $PROGRAM "' C-m
 fi
 
+# Window 2 will launch the Image-Capture node after building the ROS environment
 tmux new-window -t $SESSION:2 -n 'Image-Capture'
 if command -v docker-compose &> /dev/null; then
 tmux send-keys -t $SESSION: 'source ~/.bashrc' C-m 'PROGRAM=mock_image_capture' C-m '
@@ -56,6 +59,7 @@ source install/setup.bash &&
 ros2 run orchestrator $PROGRAM "' C-m
 fi
 
+# Window 3 will launch the Object-Detection node after building the ROS environment
 tmux new-window -t $SESSION:3 -n 'Object-Detection'
 if command -v docker-compose &> /dev/null; then
 tmux send-keys -t $SESSION: 'source ~/.bashrc' C-m 'PROGRAM=mock_object_detection' C-m '
@@ -78,5 +82,6 @@ fi
 
 tmux attach-session -t $SESSION:0
 
+# Kill the tmux session after detaching the window
 tmux kill-sess -t Project
 
