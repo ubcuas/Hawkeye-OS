2c5f87fd07ce3b27e98087de6e93875636e34538
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-02-07 11:32:33 -0800

    throw error if webrtc signaling url isn't defined

--- src/streaming/streaming/constants.py
+++ src/streaming/streaming/constants.py
@@ -1,5 +1,5 @@
 import os
 
-WEBRTC_SIGNALING_URL = os.getenv("WEBRTC_SIGNALING_URL")
-if WEBRTC_SIGNALING_URL is None:
+WEBRTC_SIGNALING_URL = str(os.getenv("WEBRTC_SIGNALING_URL"))
+if WEBRTC_SIGNALING_URL is None or WEBRTC_SIGNALING_URL == "":
     raise ValueError("WEBRTC_SIGNALING_URL environment variable is not set")
