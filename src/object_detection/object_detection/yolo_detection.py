5851b05ad56cf91164297138483cfdf1926a4240
Ram Jayakumar <ramjayakumar21@gmail.com>
2026-03-07 17:05:36 -0800

    THIS WORKS!

--- src/object_detection/object_detection/yolo_detection.py
+++ src/object_detection/object_detection/yolo_detection.py
@@ -86,6 +86,7 @@ def predict_images(node, color_msg):
     # processed images format: [((x1,y1), (x2,y2)), ...]
 
     # Run prediction on the decoded numpy array (single frame)
+    node.get_logger().info(DEVICE)
     results = model.predict(img, agnostic_nms=True, verbose=False, conf=CONF_THRESH, imgsz=IMGSZ, device=DEVICE)[0]
     output_img = img.copy()
 
